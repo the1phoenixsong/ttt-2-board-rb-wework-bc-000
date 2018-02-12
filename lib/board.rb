c003e49185b5bd5cda739cfc4550bb5e360682f0
James Santos <james.santos2@wework.com>
2018-02-12 05:19:55 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,3 @@
 # Define the variable board below.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-def display_board
-  puts "   |   |   "
-  puts "------------"
-  puts "   |   |   "
-  puts "------------"
-  puts "   |   |   "
-end
-
-display_board
